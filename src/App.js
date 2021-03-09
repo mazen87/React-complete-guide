489f7ee28dd3b08ec707c4cdbc87f1f1f8043655
mazen87 <mazen.karfoul87@hotmail.com>
2021-03-09 11:55:22 +0100

    useState hook for state manipulation-stateless and stateful components-passing method reference between components- tow way binding

--- src/App.js
+++ src/App.js
@@ -1,29 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
   state = {
-    persons : [
-      { name: 'Max' , age:28 },
-      { name: "Manue" , age:33 },
-      { name: "Stephanie" , age:32 }
-    ]
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manue', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  };
+
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manue', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
+  };
+
+  nameChangedHandler = (event)=>{
+      this.setState({
+        persons: [
+          { name: 'Max', age: 28 },
+          { name: event.target.value , age: 29 },
+          { name: 'Stephanie', age: 27 }
+        ]
+      });
   }
+
   render() {
-   return (
+    return (
       <div className="App">
-       <h1>Hi, I'm a React App </h1>
-       <p> This is really working  !</p>
-       <button>Switch Name </button>
-       <Person  name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person  name={this.state.persons[1].name} age={this.state.persons[1].age}>My hoppy is painting  </Person>
-       <Person  name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={()=>this.switchNameHandler('Maximillian')}>Switch Name</button>
+        <Person
+          click={this.switchNameHandler.bind(this, 'Maximiliano')}
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          
+        />
       </div>
     );
-
-    //return React.createElement('div',{className :'App'},React.createElement('h1',null,'Does it work?!!!!!'))
   }
+    //return React.createElement('div',{className :'App'},React.createElement('h1',null,'Does it work?!!!!!'))
+  
 }
 
 export default App;
